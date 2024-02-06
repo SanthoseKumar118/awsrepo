45ca74224edfe5d7fa6fb30d631f59f4883aeae4
Santhose <santhose@agent-network.com>
2024-02-07 00:21:54 +0900

    working code

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const express = require('express');
+const path = require('path');
 const os = require('os');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+// Serve static files from 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/get-ip', (req, res) => {
+// Route for fetching server IP
+app.get('/ip', (req, res) => {
     const networkInterfaces = os.networkInterfaces();
     for (const interfaceKey of Object.keys(networkInterfaces)) {
         for (const interface of networkInterfaces[interfaceKey]) {
             if (!interface.internal && interface.family === 'IPv4') {
-                return res.send(interface.address);
+                return res.json({ ip: interface.address });
             }
         }
     }
-    res.send('IP address not found');
+    res.status(404).json({ error: 'IP address not found' });
+});
+
+// Fallback route to serve index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
 });
